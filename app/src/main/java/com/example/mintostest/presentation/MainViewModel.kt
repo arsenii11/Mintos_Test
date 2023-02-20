b203e6a478585fd45b14a66bb186333e13988427
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2023-02-21 01:07:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mintostest/presentation/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.mintostest.presentation
+
+class MainViewModel {
+}
---
+++ app/src/main/java/com/example/mintostest/utilities/Utility.kt
@@ -0,0 +1,58 @@
+package com.example.mintostest.utilities
+
+import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import com.google.android.material.snackbar.Snackbar
+import java.util.*
+
+object Utility {
+
+
+
+
+    //Network connection check
+    fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
+    }
+
+    //custom background for snackbar
+    fun Snackbar.background(color: Int): Snackbar {
+        this.view.setBackgroundColor(color)
+        return this
+    }
+
+    fun View.visible(isVisible: Boolean) {
+        visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+
+}
